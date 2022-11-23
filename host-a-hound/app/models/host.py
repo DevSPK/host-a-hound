a3fbd16d50521a698e8863e55dfa425edab6d3fd
DevSPK <seankennedy75@gmail.com>
2022-11-23 10:04:03 -0500

    host model done

---
+++ host-a-hound/app/models/host.py
@@ -0,0 +1,44 @@
+from .db import db, environment, SCHEMA, add_prefix_for_prod
+from .user import User
+from datetime import datetime
+
+
+class Host(db.Model):
+    __tablename__ = 'hosts'
+
+    if environment == "production":
+        __table_args__ = {'schema': SCHEMA}
+
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod('users.id')))
+    name = db.Column(db.String(75), nullable=False)
+    about = db.Column(db.String(2000), nullable=False)
+    address = db.Column(db.String(150), nullable=False)
+    city = db.Column(db.String(75), nullable=False)
+    state = db.Column(db.String(25), nullable=False)
+    country = db.Column(db.String(50), nullable=False)
+    lat = db.Column(db.Float, nullable=False, default=38.907192)
+    lng = db.Column(db.Float, nullable=False, default=-77.036873)
+    price_per_night = db.Column(db.Float, nullable=False)
+    img_url= db.Column(db.String(2048), nullable=False)
+    created_at = db.Column(db.DateTime, nullable=False, default=datetime.datetime())
+    updated_at = db.Column(db.DateTime, nullable=False, default=datetime.datetime())
+
+    user = db.relationship('User', back_populates='hosts')
+
+    def to_dict(self):
+        host_dict = {
+          "id": self.id,
+          "about": self.about ,
+          "address": self.address ,
+          "city": self.city ,
+          "state": self.state ,
+          "country": self.country ,
+          "lat": self.lat ,
+          "lng": self.lng,
+          "price_per_night": self.price_per_night ,
+          "img_url": self.img_url ,
+          "created_at": self.created_at ,
+          "updated_at": self.updated_at
+        }
+        return host_dict
