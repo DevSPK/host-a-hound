635ff27774e1007f0493dfa7b8cab2c2a54f4802
DevSPK <seankennedy75@gmail.com>
2023-03-02 14:23:59 -0800

    added your-hosts component

---
+++ react-app/src/components/YourHosts/index.js
@@ -0,0 +1,52 @@
+import { useEffect, useState } from "react"
+import { useDispatch, useSelector } from "react-redux"
+import { useHistory, Link } from "react-router-dom"
+import { thunkReadAllHosts } from "../../store/host"
+import "./YourHosts.css"
+
+const YourHosts = () => {
+
+    const dispatch = useDispatch()
+    const history = useHistory()
+    // const [isLoaded, setIsLoaded] = useState(false)
+    const user = useSelector((state) => state.session.user)
+    
+    const allhosts = useSelector((state) => Object.values(state.host))
+
+    
+    useEffect( () => {
+        dispatch(thunkReadAllHosts())
+    }, [dispatch])
+    
+    // if (hosts.length === 0) return null
+    
+    
+
+    // console.log("this is user", user)
+    
+    // console.log("this is hosts in yourhosts", hosts)
+
+    // console.log("this is hosts mapped", hosts.map((host) => host))
+
+    
+    return (
+        <div className="yourhosts-wrapper">
+           
+            {hosts.map((host)=> (
+                <div key={host.id} className="yourhosts-host-card-container">
+                    <Link to={`/host/${host.id}`}>
+                    <div className="yourhosts-host-image" style={{ backgroundImage: `url(${host.img_url}), url(https://picsum.photos/seed/${host.id}/1024/720.jpg)` }}></div>
+                    <div className="yourhosts-host-info">
+                        <div className="yourhosts-host-name">{host.name}</div>
+                        <div className="yourhosts-host-city-state">{host.city}, {host.state}</div>
+                        <div className="yourhosts-host-price">${host.price_per_night.toFixed(2)} night</div>
+                    </div>
+                    </Link>
+                </div>  
+            ))}
+            </div>
+    
+);
+    };
+
+export default YourHosts
